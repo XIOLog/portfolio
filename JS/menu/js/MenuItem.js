8ef644cb9ed467796ef134d4041fd0ebadacb872
XIOLog <sodert@gmail.com>
2019-07-01 04:00:09 +0500

    update

--- JS/menu/js/MenuItem.js
+++
@@ -1,8 +0,0 @@
-function MenuItem(href, title) {
-	this.title = title;
-	this.href = href;
-}
-
-MenuItem.prototype.render = function() {
-	return '<li class="menuItem"><a href="' + this.href + '">' + this.title + '</a></li>';
-};
